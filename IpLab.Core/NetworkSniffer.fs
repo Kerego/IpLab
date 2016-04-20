68170f328ed8fc8a4774bf2a5afa1438807e7748
Marian <Marian@DESKTOP-E8BBFSG>
2016-04-21 01:56:53 +0300

    Add project files.

---
+++ IpLab.Core/NetworkSniffer.fs
@@ -0,0 +1,86 @@
+﻿namespace IpLab.Core
+open System
+open System.Collections.ObjectModel
+open System.ComponentModel
+open Microsoft.FSharp.Quotations
+open Microsoft.FSharp.Quotations.Patterns
+open System.Net.Sockets
+open System.Net
+open System.Text
+
+type IPPacket = {
+    Destination : string;
+    Source : string;
+    Version : int;
+    IHL : int;
+    DifferentiatedServices : int;
+    TotalLength : int;
+    Identification : int;
+    Flags : int;
+    FragmentOffset : int;
+    TTL : int;
+    Protocol : int;
+    HeaderChecksum : int;
+    Payload : string;
+}
+
+
+type NetworkSniffer(endpoint : IPEndPoint) =
+    let _socket : Socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP)
+    let _endpoint : IPEndPoint = endpoint
+    let inArray : array<byte> = [| byte 1; byte 0; byte 0;  byte 0|] 
+    let outArray : array<byte> = [| byte 0; byte 0; byte 0;  byte 0|]
+    do _socket.Bind(_endpoint) |> ignore
+    do _socket.IOControl(IOControlCode.ReceiveAll, inArray, outArray) |> ignore
+
+    let ExtractPayload buffer start count protocol =    
+        buffer |> 
+        Array.map(fun x -> byte x) |> 
+        Array.skip(start + match protocol with | 17 -> 8 | _ -> 0 ) |> 
+        Array.take (count - match protocol with | 17 -> 8 | _ -> 0 )
+
+
+    member x.Sniff() =
+        let bufferByte : array<byte> = Array.zeroCreate 65565
+        let receivedBytes = _socket.Receive(bufferByte)
+        let buffer = bufferByte |> Array.map(fun(x)->int x)
+        let version = buffer.[0] >>> 4
+        let ihl = buffer.[0] &&& 0x0f
+        let diffServices = buffer.[1]
+        let totalLength = (buffer.[2] <<< 8) + buffer.[3]
+        let id = (buffer.[4] <<< 8) + buffer.[5]
+        let flags = buffer.[6] >>> 5
+        let fragment = ((buffer.[6] &&& 0x1f) <<< 8) + buffer.[7]
+        let ttl = buffer.[8]
+        let protocol = buffer.[9]
+        let crc = (buffer.[10] <<< 8) + buffer.[11]
+        let source= buffer.[12].ToString() + "." + buffer.[13].ToString() + "." + buffer.[14].ToString() + "." + buffer.[15].ToString()
+        let dest = buffer.[16].ToString() + "." + buffer.[17].ToString() + "." + buffer.[18].ToString() + "." + buffer.[19].ToString()
+        
+        let payload = ExtractPayload buffer (ihl*4) (receivedBytes - 4*ihl) protocol
+        let header = match protocol with 
+                            | 17 -> "Source Port : " + ((buffer.[ihl*4] <<< 8) + buffer.[ihl*4 + 1]).ToString() + "\r\n" +
+                                    "Destination Port : " + ((buffer.[ihl*4 + 2] <<< 8) + buffer.[ihl*4 + 3]).ToString() + "\r\n" + 
+                                    "Length : " + ((buffer.[ihl*4 + 4] <<< 8) + buffer.[ihl*4 + 5]).ToString() + "\r\n" +
+                                    "Checksum : " + ((buffer.[ihl*4 + 6] <<< 8) + buffer.[ihl*4 + 7]).ToString() + "\r\n"
+                            | _ -> ""
+        let payloadWithHeader  = header + Encoding.Default.GetString(payload);
+
+        {
+            Version = version;
+            IHL = ihl;
+            DifferentiatedServices = diffServices;
+            TotalLength = totalLength;
+            Identification = id;
+            Flags = flags;
+            FragmentOffset = fragment;
+            TTL = ttl;
+            Protocol = protocol;
+            HeaderChecksum = crc;
+            Source = source;
+            Destination = dest; 
+            Payload = payloadWithHeader
+        }
+
+    member x.SniffAsync() = async { return x.Sniff()} |> Async.StartAsTask
+
---
+++ IpLab/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using IpLab.Core;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows.Threading;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using Microsoft.Win32;
+using System.IO;
+
+namespace IpLab
+{
+
+	public class ViewModel : INotifyPropertyChanged
+	{
+		private ObservableCollection<IPPacket> _filteredPackets = new ObservableCollection<IPPacket>();
+		public ObservableCollection<IPPacket> FilteredPackets
+		{
+			get { return _filteredPackets; }
+			set
+			{
+				_filteredPackets = value;
+				OnPropertyChanged();
+			}
+		}
+		private IPPacket _selectedPacket;
+		public IPPacket SelectedPacket { get { return _selectedPacket; } set { _selectedPacket = value; OnPropertyChanged(); } }
+		public List<IPPacket> Packets { get; set; } = new List<IPPacket>();
+
+		private NetworkSniffer _sniffer = new NetworkSniffer(new IPEndPoint(IPAddress.Broadcast, 0));
+
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+		private string _destinationFilter = "";
+		private string _sourceFilter = "";
+		private string _protocolFilter = "";
+
+
+		public async Task StartSniff()
+		{
+			while (true)
+			{
+				var p = await _sniffer.SniffAsync();
+				Packets.Add(p);
+				if (p.Destination.Contains(_destinationFilter) && p.Source.Contains(_sourceFilter) && p.Protocol.ToString().Contains(_protocolFilter))
+					FilteredPackets.Add(p);
+			}
+		}
+
+
+		public void FilterPackets(string filterText)
+		{
+			var filters = filterText.Split(',', ';').ToList();
+			_destinationFilter = _sourceFilter = _protocolFilter = string.Empty;
+			foreach (var filter in filters)
+			{
+				if (filter.Contains("dest:"))
+					_destinationFilter = new string(filter.Skip(filter.IndexOf(':') + 1).ToArray());
+				if (filter.Contains("source:"))
+					_sourceFilter = new string(filter.Skip(filter.IndexOf(':') + 1).ToArray());
+				if (filter.Contains("protocol:"))
+					_protocolFilter = new string(filter.Skip(filter.IndexOf(':') + 1).ToArray());
+			}
+
+			FilteredPackets.Clear();
+			foreach (var p in Packets)
+				if (p.Destination.Contains(_destinationFilter) && p.Source.Contains(_sourceFilter) && p.Protocol.ToString().Contains(_protocolFilter))
+					FilteredPackets.Add(p);
+		}
+
+	}
+
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		ViewModel _vm = new ViewModel();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.Loaded += WindowLoaded;
+		}
+
+		private async void WindowLoaded(object sender, RoutedEventArgs e)
+		{
+			DataContext = _vm;
+			await _vm.StartSniff();
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			var filterText = (sender as TextBox)?.Text ?? "";
+			_vm.FilterPackets(filterText);
+		}
+
+		private void ClearButtonClick(object sender, RoutedEventArgs e)
+		{
+			_vm.Packets.Clear();
+			_vm.FilteredPackets.Clear();
+		}
+
+		private void SaveButtonClick(object sender, RoutedEventArgs e)
+		{
+			SaveFileDialog dialog = new SaveFileDialog() { Filter = "Text documents (.txt)|*.txt", DefaultExt = ".text", FileName = "payload"};
+			bool? result = dialog.ShowDialog();
+			if(result.GetValueOrDefault() && _vm.SelectedPacket != null)
+				File.WriteAllText(dialog.FileName, _vm.SelectedPacket.Payload);
+		}
+	}
+}
